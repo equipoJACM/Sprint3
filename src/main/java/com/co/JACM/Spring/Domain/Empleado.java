c49d0bead72460bf7ae656489169883185113ada
Cristian Solarte <yovher@gmail.com>
2022-08-28 20:56:03 -0500

    Se añade lombok al Proyecto

--- src/main/java/com/co/JACM/Spring/Domain/Empleado.java
+++ src/main/java/com/co/JACM/Spring/Domain/Empleado.java
@@ -1,47 +1,13 @@
 package com.co.JACM.Spring.Domain;
 
+import lombok.Data;
+
+@Data
 public class Empleado {
     private String nombre;
     private String correo;
     private String empresaEmpleado;
     private String rolEmpleado;
 
-    public Empleado(String nombre, String correo, String empresaEmpleado, String rolEmpleado) {
-        this.nombre = nombre;
-        this.correo = correo;
-        this.empresaEmpleado = empresaEmpleado;
-        this.rolEmpleado = rolEmpleado;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getEmpresaEmpleado() {
-        return empresaEmpleado;
-    }
-
-    public void setEmpresa(String empresaEmpleado) {
-        this.empresaEmpleado = empresaEmpleado;
-    }
-
-    public String getRol() {
-        return rolEmpleado;
-    }
-
-    public void setRol(String rol) {
-        this.rolEmpleado = rolEmpleado;
-    }
 }
--- src/main/java/com/co/JACM/Spring/Domain/Empresa.java
+++ src/main/java/com/co/JACM/Spring/Domain/Empresa.java
@@ -1,49 +1,13 @@
 package com.co.JACM.Spring.Domain;
 
-public class Empresa {
+import lombok.Data;
 
+@Data
+public class Empresa {
 
     private String nombre;
     private String direccion;
     private Integer telefono;
     private Integer nit;
 
-    public Empresa(String nombre, String direccion, Integer telefono, Integer nit) {
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.nit = nit;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public Integer getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
-
-    public Integer getNit() {
-        return nit;
-    }
-
-    public void setNit(Integer nit) {
-        this.nit = nit;
-    }
 }
