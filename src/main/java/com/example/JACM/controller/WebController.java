ae7175ca112711d486c707f2ea2fb9f333c9bf57
Cristian Solarte <yovher@gmail.com>
2022-09-26 19:21:26 -0500

    Configuracion huroku

--- src/main/java/com/example/JACM/controller/WebController.java
+++ src/main/java/com/example/JACM/controller/WebController.java
@@ -1,6 +1,9 @@
 package com.example.JACM.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +13,15 @@ public class WebController {
     public String index(){
         return "index.html";
     }
+
+//    @GetMapping("/")
+//    public String index(Model model, @AuthenticationPrincipal OidcUser principal){
+//        if(principal != null){
+//            System.out.println(principal.getClaims());
+//            Autenticacion usuario = this.sauth.getOrCreateAuth(principal.getClaims());
+//            model.addAttribute("auth", usuario);
+//        }
+//        return "index";
+//    }
 
 }
